abbeeeeafbadac4a629987594fd4721e93382823
Manjula <mguneratne3@gmail.com>
2025-04-07 15:49:23 -0400

    Game reset added:

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ var monster = {
 };
 
 var monstersCaught = 0;
+
+//Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+    keysDown[e.key] = true;
+}, false);
+
+addEventListener("keyup", function (e){
+    delete keysDown[e.key];
+}, false);
+
+//Reset the game when the player catches a monster
+var reset = function() {
+    hero.x = canvas.width/2;
+    hero.y = canvas.height/2;
+
+    //Throw the monster somewhere on the screen randomly
+    monster.x = 32 + (Math.random()*(canvas.width - 64));
+    monster.y = 32 + (Math.random()*(canvas.height - 64));
+}
